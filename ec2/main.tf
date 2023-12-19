9c6e2543a66922a0eba8aa10fdfc9716d3483fb3
Chandra <g1.smikcp@gmail.com>
2023-12-19 20:42:47 +0530

    New script for vpc and ec2

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,6 +1,6 @@
-provider "aws" {
-  region = "us-east-1"  # Change to your desired AWS region
-}
+# provider "aws" {
+#   region = "us-east-1"  # Change to your desired AWS region
+# }
 
 module "vpc" {
   source = "./../vpc"  # Adjust the path based on your actual directory structure

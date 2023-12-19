438165581c4f0dfdd31044af5ddba864585a862e
Chandra <g1.smikcp@gmail.com>
2023-12-19 21:54:53 +0530

    New script for vpc and ec2

--- ec2/output.tf
+++ ec2/output.tf
@@ -1,7 +1,3 @@
-# output "my_vpc_id" {
-#   value = module.vpc.aws_vpc.my_vpc
-# }
-
 output "ec2_instance_id" {
   value = aws_instance.my_instance.id
   
@@ -10,17 +6,4 @@ output "ec2_instance_id" {
 
 output "ec2_public_ip" {
   value = aws_instance.my_instance.public_ip
-}
-
-
-output "my_vpc_id" {
-  value = aws_vpc.my_vpc.id
-}
-
-output "my_sg_id" {
-  value = aws_security_group.my_sg.id
-}
-
-output "my_subnet_id" {
-  value = aws_subnet.my_subnet.id
 }
--- ec2/provider.tf
+++ ec2/provider.tf
@@ -1,6 +1,3 @@
-provider "aws" {
-  region = "us-east-1"
-}
 
 terraform {
   required_version = ">= 1.0"

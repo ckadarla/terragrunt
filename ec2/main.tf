4d023cf55440fe770ca7a5c9d80bd63afb0b6792
Chandra <g1.smikcp@gmail.com>
2023-12-19 20:40:00 +0530

    New script for vpc and ec2

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,33 +1,19 @@
-# provider "aws" {
-#   region = "us-east-1"  # Change to your desired AWS region
-# }
+provider "aws" {
+  region = "us-east-1"  # Change to your desired AWS region
+}
+
+module "vpc" {
+  source = "./../vpc"  # Adjust the path based on your actual directory structure
+}
 
 resource "aws_instance" "my_instance" {
   ami           = "ami-0c55b159cbfafe1f0"  # Change to your desired AMI
   instance_type = "t2.micro"
 
-  vpc_security_group_ids = [aws_security_group.my_sg.id]
-  subnet_id              = aws_subnet.my_subnet.id
+  vpc_security_group_ids = [module.vpc.my_sg.id]
+  subnet_id              = module.vpc.my_subnet.id
 
   tags = {
     Name = "MyInstance"
   }
-}
-
-resource "aws_security_group" "my_sg" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_subnet" "my_subnet" {
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"  # Change to your desired AZ
-  map_public_ip_on_launch = true
 }
--- vpc/main.tf
+++ vpc/main.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = "us-east-1"  # Change to your desired AWS region
-}
-
 resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true
@@ -10,4 +6,30 @@ resource "aws_vpc" "my_vpc" {
   tags = {
     Name = "MyVPC"
   }
+}
+
+resource "aws_security_group" "my_sg" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"  # Change to your desired AZ
+  map_public_ip_on_launch = true
+}
+
+output "my_sg" {
+  value = aws_security_group.my_sg
+}
+
+output "my_subnet" {
+  value = aws_subnet.my_subnet
 }

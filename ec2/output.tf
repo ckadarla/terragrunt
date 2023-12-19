5cb114d516bfd2284c42f09c004d039c943c0fa1
Chandra <g1.smikcp@gmail.com>
2023-12-19 21:51:27 +0530

    New script for vpc and ec2

--- ec2/output.tf
+++ ec2/output.tf
@@ -11,3 +11,16 @@ output "ec2_instance_id" {
 output "ec2_public_ip" {
   value = aws_instance.my_instance.public_ip
 }
+
+
+output "my_vpc_id" {
+  value = aws_vpc.my_vpc.id
+}
+
+output "my_sg_id" {
+  value = aws_security_group.my_sg.id
+}
+
+output "my_subnet_id" {
+  value = aws_subnet.my_subnet.id
+}

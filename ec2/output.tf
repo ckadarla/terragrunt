c9f61d0c378921c61be4bc1850bcb25363184bd6
Chandra <g1.smikcp@gmail.com>
2023-12-19 20:50:25 +0530

    New script for vpc and ec2

---
+++ ec2/output.tf
@@ -0,0 +1,11 @@
+output "vpc_id" {
+  value = module.vpc.my_vpc.id
+}
+
+output "ec2_instance_id" {
+  value = aws_instance.my_instance.id
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.my_instance.public_ip
+}

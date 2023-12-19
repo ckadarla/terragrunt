2a72aa9acda5d1129da0813250f952729b8f6282
Chandra <g1.smikcp@gmail.com>
2023-12-19 21:30:01 +0530

    New script for vpc and ec2

--- ec2/output.tf
+++ ec2/output.tf
@@ -1,10 +1,10 @@
-# output "vpc_id" {
-#   value = module.vpc.my_vpc.id
-# }
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
 
 output "ec2_instance_id" {
   value = aws_instance.my_instance.id
-  value = module.vpc.vpc_id
+  
   
 }
 

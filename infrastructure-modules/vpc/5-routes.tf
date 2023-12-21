91d1d01e8dda0643c490d93ff0db7e48ec861642
Chandra <g1.smikcp@gmail.com>
2023-12-21 19:00:46 +0530

    New script for eks

--- infrastructure-modules/vpc/5-routes.tf
+++ infrastructure-modules/vpc/5-routes.tf
@@ -1,10 +1,10 @@
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.this.id
 
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.this.id
-  }
+  # route {
+  #   cidr_block     = "0.0.0.0/0"
+  #   nat_gateway_id = aws_nat_gateway.this.id
+  # }
 
   tags = {
     Name = "${var.env}-private"

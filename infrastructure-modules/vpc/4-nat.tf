f642c1d4cf05d010c3402f404fc20f24ff86c005
Chandra <g1.smikcp@gmail.com>
2023-12-21 18:57:18 +0530

    New script for eks

--- infrastructure-modules/vpc/4-nat.tf
+++ infrastructure-modules/vpc/4-nat.tf
@@ -1,18 +1,18 @@
-resource "aws_eip" "this" {
-  vpc = true
+# resource "aws_eip" "this" {
+#   vpc = true
 
-  tags = {
-    Name = "${var.env}-nat"
-  }
-}
+#   tags = {
+#     Name = "${var.env}-nat"
+#   }
+# }
 
-resource "aws_nat_gateway" "this" {
-  allocation_id = aws_eip.this.id
-  subnet_id     = aws_subnet.public[0].id
+# resource "aws_nat_gateway" "this" {
+#   allocation_id = aws_eip.this.id
+#   subnet_id     = aws_subnet.public[0].id
 
-  tags = {
-    Name = "${var.env}-nat"
-  }
+#   tags = {
+#     Name = "${var.env}-nat"
+#   }
 
-  depends_on = [aws_internet_gateway.this]
-}
+#   depends_on = [aws_internet_gateway.this]
+# }
